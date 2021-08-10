a768b2c31296c114e811855975c1bf0acb0a4381
Emanuel Vargas <silverzero55@gmail.com>
2021-08-10 16:35:27 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { Server as WebSocketServer } from "socket.io";
+import http from "http";
+import { v4 as uuid } from "uuid";
+
+const notes = [];
+
+const app = express();
+const server = http.createServer(app);
+const io = new WebSocketServer(server);
+
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+  console.log("Nueva conexion: ", socket.id);
+
+  socket.emit("server:loadnotes", notes);
+
+  socket.on("client:newnote", (newNote) => {
+    const note = {
+      ...newNote,
+      id: uuid(),
+    };
+
+    notes.push(note);
+
+    socket.emit("server:newnote", note);
+  });
+});
+
+server.listen(3000);
+
+console.log("Server on port", 3000);
